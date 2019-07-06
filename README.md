3caa0f66d85f171e42516bb3c6799e2689e3d3cd
sanchit_singh <sanchitsingh0409@gmail.com>
2019-07-07 04:16:47 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,85 @@
+# [MIS React UI](MIS)
+Light Bootstrap Dashboard React
+├── README.md
+├── package.json
+├── public
+│   ├── apple-icon.png
+│   ├── favicon.ico
+│   ├── index.html
+│   ├── manifest.json
+│   └── sidebar-5.jpg
+└── src
+    ├── index.js (10)
+    ├── assets              page 1              page 2
+    │   ├── css             div  /                   div /
+    │   ├── fonts           div  /                 section ~
+    │   ├── img             section ~                div /
+    ├── components
+    │   ├── Card
+    │   │   └── Card.jsx
+    │   ├── CustomButton
+    │   │   └── CustomButton.jsx
+    │   ├── Footer
+    │   │   └── Footer.jsx
+    │   ├── FormDropdown
+    │   │   └── FormDropdown.jsx
+    │   ├── FormInputs
+    │   │   └── FormInputs.jsx
+    │   ├── Header
+    │   │   ├── Header.jsx
+    │   │   └── HeaderLinks.jsx
+    │   ├── Sidebar
+    │   │   └── Sidebar.jsx
+    │   ├── StatsCard
+    │   │   └── StatsCard.jsx
+    │   └── UserCard
+    │       └── UserCard.jsx
+    ├── layouts
+    │   └── Verification
+    │       └── Verification.jsx
+    ├── routes
+    │   ├── authentication.jsx
+    │   └── index.jsx
+    │   └── MainRoutes.jsx
+    ├── variables
+    │   └── Variables.jsx
+    └── views
+        ├── Dashboard
+        │   └── Dashboard.jsx
+        ├── Family
+        ├── Family
+        │   └── components
+        |   |       |── AddFamily
+        |   |       │   └── AddFamily.jsx
+        |   |       │   └── Asset.jsx
+        |   |       │   └── Expense.jsx
+        |   |       │   └── Income.jsx
+        |   |       │   └── Investment.jsx
+        |   |       │   └── PrimaryInfo.jsx
+        |   |       |── AddMember.jsx
+        |   |       │   └── AddMember.jsx
+        |   |       │   └── BusinessChallenges.jsx
+        |   |       │   └── BusinessDetails.jsx
+        |   |       │   └── PrimaryInfo.jsx
+        |   |       │   └── SalaryDetails.jsx
+        |   |       |── ShowAllFamily
+        |   |       │   └── ShowAllFamily.jsx
+        |   |       |── ShowAllMembers
+        |   |       |  └── Asset.jsx
+        |   |       |  └── Expense.jsx
+        |   |       |  └── Income.jsx
+        |   |       |  └── Investment.jsx
+        |   |       |  └── Member.jsx
+        |   |       |  └── PrimaryInfo.jsx
+        |   |       |  └── ShowAllMembers.jsx
+        │   └── Family.jsx
+        │   └── FamilyDetails.jsx
+        ├── Universe
+        │   └── Universe.jsx
+        ├── Programs
+        │   └── Programs.jsx
+        |── Reports
+        |   └── Reports.jsx
+        └── Verification
+            └── Authentication.jsx
+            └── Login.jsx
---
+++ main.py
@@ -0,0 +1,150 @@
+import json
+import glob
+from flask import Flask, render_template, request
+import webbrowser
+import argparse
+
+app = Flask(__name__)
+# configuring folder path for loading all json
+path = 'static/milk.com/*.json'
+# fetching the file names of all json present in the folder
+files = glob.glob(path)
+output = []  # final output
+outputForTree = []
+hierachies = []  # storing temporary hierachies of pages
+parent = []  # storing temporary parent record of each page
+# configuring arguments for the script to take as input
+ap = argparse.ArgumentParser()
+ap.add_argument('-u', '--url', required=True,
+                help='website url to analyse layout')
+ap.add_argument('-l', '--level', required=True,
+                help='what should be depth level to check')
+ap.add_argument('-m', '--max', required=False,
+                help='maximum number of pages to scrap')
+args = ap.parse_args()
+
+
+# recursive function to extract tags from each json, do not alter as it uses global variable
+def extractGivenLevelTags(obj, level):
+    global parent
+    if level == 1:
+        for items in obj['child']:
+            parent.append(obj['tag'])
+            hierachies.append({"parent": parent.copy(), "child": items['tag']})
+            parent.pop()
+        return
+    if len(obj['child']):
+        parent.append(obj['tag'])
+    else:
+        return
+    for items in obj['child']:
+        extractGivenLevelTags(items, level-1)
+    if len(parent):
+        parent.pop()
+
+
+# compares two layout and generate the final output
+def compareLayout(file1, file2, data1, data2):
+    final_data1 = []  # used to store unique tags on given level with different parents
+    final_data2 = []
+    for value in data1:  # logic to remove duplicate tags with similar parents
+        if value not in final_data1:
+            final_data1.append(value)
+    for value in data2:
+        if value not in final_data2:
+            final_data2.append(value)
+    # extract similar tags between two page and dissimilar tags for each page
+    similarTags = [d for d in final_data1 if d in final_data2]
+    dissimilarTagsForData1 = [d for d in final_data1 if d not in similarTags]
+    dissimilarTagsForData2 = [d for d in final_data2 if d not in similarTags]
+    # computing similarity of percentage between two page
+    try:
+        percentageSimilarity = (len(
+            similarTags) / (len(dissimilarTagsForData1) + len(dissimilarTagsForData2))) * 100
+    except:
+        percentageSimilarity = 100
+    # appending every object to final output
+    output.append({"pages": file1.split('\\')[1].split('.')[
+                  0] + " and " + file2.split('\\')[1].split('.')[0], "similarHierarchies": similarTags, "dissimilarTags": [
+        {"name": file1.split('\\')[1].split('.')[
+            0], "tags": dissimilarTagsForData1},
+        {"name": file2.split('\\')[1].split(
+            '.')[0], "tags": dissimilarTagsForData2}
+    ], "percentageSimilarity": percentageSimilarity})
+
+
+# extracting each page and comparing it with all other pages.
+def startWorker(level):
+    for i in range(len(files)):
+        hierachies.clear()
+        with open(files[i], "r", encoding="utf8") as read_file:
+            extractGivenLevelTags(json.load(read_file), level-1)
+        first_data = hierachies[:]
+        hierachies.clear()
+        for j in range(i+1, len(files)):
+            with open(files[j], "r", encoding="utf8") as read_file:
+                extractGivenLevelTags(json.load(read_file), level-1)
+            second_data = hierachies[:]
+            hierachies.clear()
+            compareLayout(files[i], files[j], first_data, second_data)
+    return output
+
+
+x = startWorker(int(args.level))
+
+
+@app.route('/')
+def dashboard():
+    return render_template('dashboard.html', url=args.url, level=args.level, maxpages=args.max,  filterValue=0, data=x)
+
+
+@app.route('/', methods=['POST'])
+def filter():
+    return render_template('dashboard.html', url=args.url, level=args.level, maxpages=args.max,  filterValue=int(request.form['filter']), data=x)
+
+
+@app.route('/tree/<pageName>')
+def showTree(pageName):
+    page1TreeTemp = []
+    page2TreeTemp = []
+    page1Dict = []
+    page2Dict = []
+    page1 = pageName.split(' ', 1)[0]
+    page2 = pageName.split(' ', 2)[2]
+
+    def dictToList(obj):  # converts list of dicts to a single list
+        res = []
+        for z in obj:
+            for k, v in z.items():
+                if(isinstance(v, list)):
+                    for i in v:
+                        res.append(i)
+                else:
+                    res.append(v)
+        return res
+    for file in files:
+        if file.split('\\')[1].split('.')[0] == page1:
+            with open(file, "r", encoding="utf8") as read_file:
+                hierachies.clear()
+                extractGivenLevelTags(json.load(read_file), int(args.level)-1)
+                page1TreeTemp = hierachies[:]
+        if file.split('\\')[1].split('.')[0] == page2:
+            with open(file, "r", encoding="utf8") as read_file:
+                hierachies.clear()
+                extractGivenLevelTags(json.load(read_file), int(args.level)-1)
+                page2TreeTemp = hierachies[:]
+        for value in page1TreeTemp:  # logic to remove duplicate tags with similar parents
+            if value not in page1Dict:
+                page1Dict.append(value)
+        for value in page2TreeTemp:
+            if value not in page2Dict:
+                page2Dict.append(value)
+    page1List = dictToList(page1Dict)
+    page2List = dictToList(page2Dict)
+    listData = [page1List, page2List]
+    return render_template('testtree.html', url=args.url, level=args.level, maxpages=args.max, pageName=pageName, data=json.dumps(listData))
+
+
+if __name__ == '__main__':
+    webbrowser.open('http://127.0.0.1:5000/')
+    app.run(debug=True)
